a1700b2e72c0899d21572d2d873cb78de122c1ea
Howard Kwon <howie.ykwon70@gmail.com>
2021-08-30 11:14:06 -0400

    commit tree 8cf82066e11decca70e72c318d990f75999c6208

---
+++ app.rb
@@ -0,0 +1,26 @@
+# Console.logs and puts line breaks
+# puts "Hello world!"
+# puts "Hello world!"
+# puts "Hello world!"
+
+puts "Hello World!"
+
+# # console.logs, does not put line breaks
+# print "Hello world!"
+# print "Hello world!"
+# print "Hello world!"
+
+print "Pass this test, please."
+
+# p method helps console an array by calling the .inspect method on it
+# ruby will try to coerce data to a string by calling the .to_s method
+p [1, 2, 3]
+
+
+# Ruby has one more method, pp, for "pretty-printing" complex data, 
+# such as nested arrays and hashes (the Ruby equivalent of a JavaScript object)
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+
+
+  
